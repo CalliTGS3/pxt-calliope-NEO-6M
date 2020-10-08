cf453247dd43d7255fd3790120316934f40129ae
Raik Andritschke <58722460+CalliTGS3@users.noreply.github.com>
2020-10-08 23:53:49 +0200

    Update NEO-6M.ts
    export function writeConfig

--- NEO-6M.ts
+++ NEO-6M.ts
@@ -49,6 +49,12 @@ namespace NEO6M_GPS {
         return;
     }
 
+    //% blockId="writeConfig" block="Schreibe Konfiguration %buffer"
+    //% advanced=true
+    export function writeConfig(buffer: Buffer): void {
+        writeBuffer(buffer);
+    }
+
     //% blockId="initGPS" block="Initialisiere Serielle Schnittstelle mit TX Pin %tx | RX Pin %rx | Baudrate %baud"
     //% tx.defl=SerialPin.C17
     //% rx.defl=SerialPin.C16
