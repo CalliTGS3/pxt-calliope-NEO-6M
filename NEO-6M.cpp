0d99d6688ccb0d5b7d83c411c0c3938f8f813550
Raik Andritschke <58722460+CalliTGS3@users.noreply.github.com>
2020-10-09 16:47:59 +0200

    Update NEO-6M.cpp

--- NEO-6M.cpp
+++ NEO-6M.cpp
@@ -9,6 +9,6 @@ namespace NEO6M_GPS {
     //%
     void writeBuffer(Buffer buffer, int size) {
         if (!buffer) return;
-        uBit.serial.send(buffer, size);
+        uBit.serial.send((unit8_t *)buffer, size);
     }    
 }
